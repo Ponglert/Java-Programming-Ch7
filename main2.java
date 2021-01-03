1d6f19e32453c8ab41dfc402a2eec727d0a43e96
Ponglert <71001516+Ponglert@users.noreply.github.com>
2021-01-04 05:06:32 +0700

    Add files via upload

---
+++ main2.java
@@ -0,0 +1,12 @@
+package testch7_1;
+
+public class main2 {
+
+	public static void main(String[] args) {
+		ExampleStatic.staticMethod();
+		ExampleStatic object = new ExampleStatic();
+		object.nonStaticMethod();
+
+	}
+
+}
