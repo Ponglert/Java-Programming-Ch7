ed1496cdd3a3da329470d58c8353d955e9620ff5
Ponglert <71001516+Ponglert@users.noreply.github.com>
2021-01-04 10:41:54 +0700

    Add files via upload

--- ExampleStatic.java
+++ ExampleStatic.java
@@ -3,11 +3,15 @@ package testch7_1;
 public class ExampleStatic {
 	private  int nonStaticVariable;
 	private  static int staticVariable;
+	private  final int finalVariable = 100;
 
     public static void staticMethod() {   
          staticVariable = 9999;
          System.out.println("Set StaticVariable = " + staticVariable);
     }
+    public void finalMethod() {
+        System.out.println("finalVariable = " + finalVariable);
+   }
     public void nonStaticMethod() {
          nonStaticVariable = 999;
          staticVariable = 9998;
