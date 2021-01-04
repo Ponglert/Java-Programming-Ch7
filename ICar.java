079210243ed3a811622d623cc6195203f3c06185
Ponglert <71001516+Ponglert@users.noreply.github.com>
2021-01-04 11:00:05 +0700

    Update ICar.java

--- ICar.java
+++ ICar.java
@@ -2,7 +2,7 @@ package testch7_1;
 
 public interface  ICar {
 	public void engineStart();
-    public void changeGear(int newGear);
-    public void turnLeft();
-    public void turnRight();
+    	public void changeGear(int newGear);
+    	public void turnLeft();
+    	public void turnRight();
 }
