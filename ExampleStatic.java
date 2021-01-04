c165350737f04a58f735eec4d773ba17770f441d
Ponglert <71001516+Ponglert@users.noreply.github.com>
2021-01-04 10:42:31 +0700

    Update ExampleStatic.java

--- ExampleStatic.java
+++ ExampleStatic.java
@@ -5,17 +5,17 @@ public class ExampleStatic {
 	private  static int staticVariable;
 	private  final int finalVariable = 100;
 
-    public static void staticMethod() {   
-         staticVariable = 9999;
-         System.out.println("Set StaticVariable = " + staticVariable);
-    }
-    public void finalMethod() {
-        System.out.println("finalVariable = " + finalVariable);
-   }
-    public void nonStaticMethod() {
-         nonStaticVariable = 999;
-         staticVariable = 9998;
-         System.out.println("Set nonStaticVariable = " + nonStaticVariable);
-         System.out.println("Set staticVariable = " + staticVariable);
-    }
+	public static void staticMethod() {   
+		staticVariable = 9999;
+		System.out.println("Set StaticVariable = " + staticVariable);
+	}
+	public void finalMethod() {
+		System.out.println("finalVariable = " + finalVariable);
+	}
+	public void nonStaticMethod() {
+		nonStaticVariable = 999;
+		staticVariable = 9998;
+		System.out.println("Set nonStaticVariable = " + nonStaticVariable);
+		System.out.println("Set staticVariable = " + staticVariable);
+	}
 }
