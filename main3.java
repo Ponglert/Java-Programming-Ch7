703402ade51b8f2fe094361722215182fad66f54
Ponglert <71001516+Ponglert@users.noreply.github.com>
2021-01-04 08:10:47 +0700

    Update main3.java

--- main3.java
+++ main3.java
@@ -4,13 +4,13 @@ public class main3 {
 
 	public static void main(String[] args) {
 		Fruit fr = new Fruit();
-        fr.name = "Grape";
-        fr.flavor = "sour";
-        fr.setColor("green");
+		fr.name = "Grape";
+		fr.flavor = "sour";
+		fr.setColor("green");
 
-        System.out.println("Fruit name: " + fr.name);
-        System.out.println("Flavor: " + fr.flavor);
-        System.out.println("Color: " + fr.getColor());
+		System.out.println("Fruit name: " + fr.name);
+		System.out.println("Flavor: " + fr.flavor);
+		System.out.println("Color: " + fr.getColor());
 
 	}
 
